80cddcc5c61243af8b2da510ab10c664c18cf260
MateuszRuczaj1 <MateuszRuczaj1>
2026-02-13 19:21:18 +0100

    chore: update index for image extraction work

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("Hello, World!");
+
+function main(){
+  const argv = process.argv
+  const CLI_args = argv.slice(2)
+  argv.forEach((val, index) => {
+    console.log(`${index}: ${val}`)
+  })
+  if(CLI_args.length != 1){
+    console.log("One runtime argument required")
+    process.exit(1)
+  }
+  else{
+     console.log("Crawler is starting with URL: ", CLI_args[0])
+     process.exit(0)
+  }
+}
+main()
